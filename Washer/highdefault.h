6a178a206bcd20b226070919ffcc0b69db95a449
LeonelNL <leonelnieto305@gmail.com>
2022-10-21 18:46:17 -0500

    Se agrega la maquina de estados de los leds

---
+++ Washer/highdefault.h
@@ -0,0 +1,35 @@
+#include <stdbool.h>
+#include "inc/hw_types.h"
+#include "inc/hw_memmap.h"
+#include "driverlib/sysctl.h"
+#include "driverlib/gpio.h"
+
+#define CycleLeds GPIO_PIN_0|GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3|GPIO_PIN_4
+uint8_t CurrentState;
+
+void HighCycleLedDefault(CurrentState){
+    switch(CurrentState){
+        case 1:                                                 // IDLE LEDS OFF
+            GPIOPinWrite(GPIO_PORTF_BASE, CycleLeds, 0);
+            break;
+        case 2:                                                 // Boton Start, High (Ciclo 1) LED CICLO 1 ON
+            GPIOPinWrite(GPIO_PORTF_BASE, CycleLeds, 2);
+            break;
+        case 3:                                                 // Boton ciclo 2 LED CICLO 2 ON
+            GPIOPinWrite(GPIO_PORTF_BASE, CycleLeds, 4);
+            break;
+        case 4:                                                 // Boton ciclo 3 LED CICLO 3 ON
+            GPIOPinWrite(GPIO_PORTF_BASE, CycleLeds, 8);
+            break;
+        case 5:                                                 // Boton ciclo 4 LED CICLO 4 ON
+            GPIOPinWrite(GPIO_PORTF_BASE, CycleLeds, 16);
+            break;
+        case 6:                                                 // Cycle run, LED START ON
+            GPIOPinWrite(GPIO_PORTF_BASE, CycleLeds, 1);
+            break;
+        default:
+            GPIOPinWrite(GPIO_PORTF_BASE, CycleLeds, 1);
+            break;
+    }
+}
+
--- Washer/main.c
+++ Washer/main.c
@@ -4,14 +4,24 @@
 #include "inc/hw_memmap.h"
 #include "driverlib/sysctl.h"
 #include "driverlib/gpio.h"
+#include "highdefault.h"
+
+#define CycleLeds GPIO_PIN_0|GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3|GPIO_PIN_4
+
+uint8_t CurrentState = 0;
+
 
 int main(void)
 {
     SysCtlClockSet(SYSCTL_SYSDIV_5|SYSCTL_USE_PLL|SYSCTL_XTAL_16MHZ|SYSCTL_OSC_MAIN);
     SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF);
-    GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE, GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3);
-    while(1)
+    GPIOUnlockPin(GPIO_PORTF_BASE, GPIO_PIN_0);
+    GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE, GPIO_PIN_0|GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3|GPIO_PIN_4);
+   while(1)
     {
-        GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3, 0);
+        CurrentState ++;
+        HighCycleLedDefault(CurrentState);
+        SysCtlDelay(3000000);
     }
+    //GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3, 0);
 }
