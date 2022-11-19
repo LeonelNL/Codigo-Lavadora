feab3d3878997da0495c96816c4d1196e737a0a7
LeonelNL <leonelnieto305@gmail.com>
2022-11-18 21:27:31 -0600

    Se limpian banderas y creacion variable cicloseleccionado

--- Washer/config_interrupcion.h
+++ Washer/config_interrupcion.h
@@ -12,10 +12,10 @@
 
 #define CycleButtons GPIO_PIN_0|GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3|GPIO_PIN_4
 uint8_t state = 0;
-uint8_t BotonPulsado1 = 0 , BotonHigh1 = 0, BotonMedium1 = 0, BotonLow1 = 0, BotonAU1 = 0;
-uint8_t PhaseMachine;
+uint8_t CicloSeleccionado = 0;
+uint8_t BotonPulsado1;
 
-void GPIOIntHandler()
+void GPIOIntHandler(uint8_t *PhaseState)
 {
     GPIOIntClear(GPIO_PORTB_BASE, GPIO_INT_PIN_0|GPIO_INT_PIN_1|GPIO_INT_PIN_2|GPIO_INT_PIN_3|GPIO_INT_PIN_4);
     state = GPIOIntStatus(GPIO_PORTB_BASE, true);
@@ -24,46 +24,26 @@ void GPIOIntHandler()
     {
             case 1:                                                                 //BotonStart
                 BotonPulsado1 = 2;
-                if(BotonHigh1 == 1)
-                {
-                    BotonPulsado1 = 6;
-                }
-                else if(BotonMedium1 == 1)
-                {
-                    BotonPulsado1 = 7;
-                }
-                else if(BotonLow1 == 1)
-                {
-                    BotonPulsado1 = 8;
-                }
-                else if(BotonAU1 == 1)
-                {
-                    BotonPulsado1 = 9;
-                }
-                else if (PhaseMachine == 2 || 3)
-                {
-                    BotonPulsado1 = 2;
-                }
                 break;
+
             case 2:                                                                //Boton HIGH
                 BotonPulsado1 = 1;
-                BotonHigh1 = 1;
-                BotonMedium1 = 0, BotonLow1 = 0, BotonAU1 = 0;
+                CicloSeleccionado = CicloHigh;
                 break;
+
             case 4:                                                               //Boton MEDIUM
                 BotonPulsado1 = 3;
-                BotonMedium1 = 1;
-                BotonHigh1 = 0, BotonLow1 = 0, BotonAU1 = 0;
+                CicloSeleccionado = CicloMedium;
                 break;
+
             case 8:                                                              //Boton LOW
                 BotonPulsado1 = 4;
-                BotonLow1 = 1;
-                BotonHigh1 = 0, BotonMedium1 = 0, BotonAU1 = 0;
+                CicloSeleccionado = CicloLow;
                 break;
+
             case 16:                                                             //Boton CICLO AU
                 BotonPulsado1 = 5;
-                BotonAU1 = 1;
-                BotonHigh1 = 0, BotonLow1 = 0, BotonMedium1 = 0;
+                CicloSeleccionado = CicloAU;
                 break;
     }
 }
--- Washer/standby_state.h
+++ Washer/standby_state.h
@@ -6,8 +6,9 @@
 #include "definiciones.h"
 
 uint8_t PhaseMachine;
+uint8_t CicloIniciado;
 
-void Standby_State(uint8_t BotonPulsadoStandby)
+void Standby_State(uint8_t BotonPulsadoStandby, uint8_t CicloSeleccionado)
 {
     switch(BotonPulsadoStandby)
     {
@@ -15,7 +16,7 @@ void Standby_State(uint8_t BotonPulsadoStandby)
             GPIOPinWrite(GPIO_PORTF_BASE, CycleLeds, 16);
             break;
         case 2:                                                 // BOTON START
-            GPIOPinWrite(GPIO_PORTF_BASE, CycleLeds, 2);
+            PhaseMachine = RunState;
             break;
         case 3:                                                 // Bonton MEDIUM
             GPIOPinWrite(GPIO_PORTF_BASE, CycleLeds, 8);
@@ -26,21 +27,6 @@ void Standby_State(uint8_t BotonPulsadoStandby)
         case 5:                                                 // Boton AU
             GPIOPinWrite(GPIO_PORTF_BASE, CycleLeds, 2);
             break;
-        case 6:                                                 // Boton AU
-            GPIOPinWrite(GPIO_PORTF_BASE, CycleLeds, 17);
-            PhaseMachine = RunState;
-            break;
-        case 7:                                                 // Boton AU
-            GPIOPinWrite(GPIO_PORTF_BASE, CycleLeds, 9);
-            PhaseMachine = RunState;
-            break;
-        case 8:                                                 // Boton AU
-            GPIOPinWrite(GPIO_PORTF_BASE, CycleLeds, 5);
-            PhaseMachine = RunState;
-            break;
-        case 9:                                                 // Boton AU
-            GPIOPinWrite(GPIO_PORTF_BASE, CycleLeds, 3);
-            PhaseMachine = RunState;
-            break;
     }
+    CicloIniciado = CicloSeleccionado;
 }
