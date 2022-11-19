11101bbb1f650c90d8fab8460f4684fa9810ceaf
Leonel <110416710+LeonelNL@users.noreply.github.com>
2022-11-18 21:40:31 -0600

    Revert "Limpiar banderas y creación variable cicloseleccionado"

--- Washer/config_interrupcion.h
+++ Washer/config_interrupcion.h
@@ -12,10 +12,10 @@
 
 #define CycleButtons GPIO_PIN_0|GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3|GPIO_PIN_4
 uint8_t state = 0;
-uint8_t CicloSeleccionado = 0;
-uint8_t BotonPulsado1;
+uint8_t BotonPulsado1 = 0 , BotonHigh1 = 0, BotonMedium1 = 0, BotonLow1 = 0, BotonAU1 = 0;
+uint8_t PhaseMachine;
 
-void GPIOIntHandler(uint8_t *PhaseState)
+void GPIOIntHandler()
 {
     GPIOIntClear(GPIO_PORTB_BASE, GPIO_INT_PIN_0|GPIO_INT_PIN_1|GPIO_INT_PIN_2|GPIO_INT_PIN_3|GPIO_INT_PIN_4);
     state = GPIOIntStatus(GPIO_PORTB_BASE, true);
@@ -24,26 +24,46 @@ void GPIOIntHandler(uint8_t *PhaseState)
     {
             case 1:                                                                 //BotonStart
                 BotonPulsado1 = 2;
+                if(BotonHigh1 == 1)
+                {
+                    BotonPulsado1 = 6;
+                }
+                else if(BotonMedium1 == 1)
+                {
+                    BotonPulsado1 = 7;
+                }
+                else if(BotonLow1 == 1)
+                {
+                    BotonPulsado1 = 8;
+                }
+                else if(BotonAU1 == 1)
+                {
+                    BotonPulsado1 = 9;
+                }
+                else if (PhaseMachine == 2 || 3)
+                {
+                    BotonPulsado1 = 2;
+                }
                 break;
-
             case 2:                                                                //Boton HIGH
                 BotonPulsado1 = 1;
-                CicloSeleccionado = CicloHigh;
+                BotonHigh1 = 1;
+                BotonMedium1 = 0, BotonLow1 = 0, BotonAU1 = 0;
                 break;
-
             case 4:                                                               //Boton MEDIUM
                 BotonPulsado1 = 3;
-                CicloSeleccionado = CicloMedium;
+                BotonMedium1 = 1;
+                BotonHigh1 = 0, BotonLow1 = 0, BotonAU1 = 0;
                 break;
-
             case 8:                                                              //Boton LOW
                 BotonPulsado1 = 4;
-                CicloSeleccionado = CicloLow;
+                BotonLow1 = 1;
+                BotonHigh1 = 0, BotonMedium1 = 0, BotonAU1 = 0;
                 break;
-
             case 16:                                                             //Boton CICLO AU
                 BotonPulsado1 = 5;
-                CicloSeleccionado = CicloAU;
+                BotonAU1 = 1;
+                BotonHigh1 = 0, BotonLow1 = 0, BotonMedium1 = 0;
                 break;
     }
 }
